ff7a07864da90271494ae006e164f1c2d088f34d
jason <jason@localhost>
2020-01-27 13:20:44 +0800

    1、子类重写父类的方法 2、编写方法时，使用个父类定义的方法 3、运行时，根据实际创建的对象类型动态决定使用哪个方法。

--- src/com/six/two/TestPrinter.java
+++ src/com/six/two/TestPrinter.java
@@ -20,17 +20,30 @@ class WPrinter extends Printer{
 }
 /* 控制打印输出 */
 class PrintHandle{
-	public void testCPinter(int id) {
-		if (id == 1) {
-		//测试彩色打印机是否工作
-		CPrinter cprinter = new CPrinter();
-		cprinter.paint();
-		}else {
-			//测试黑白打印机是否工作
-			WPrinter wprinter = new WPrinter();
-			wprinter.paint();
-		}
+//	public void testCPinter(int id) {
+//		if (id == 1) {
+//		//测试彩色打印机是否工作
+//		CPrinter cprinter = new CPrinter();
+//		cprinter.paint();
+//		}else {
+//			//测试黑白打印机是否工作
+//			WPrinter wprinter = new WPrinter();
+//			wprinter.paint();
+//		}
+//	}
+//	//测试彩色打印机是否工作
+//	public void testPinter(CPrinter cprinter) {
+//		cprinter.paint();
+//	}
+//	//测试黑白打印机是否工作
+//	public void testPinter(WPrinter wprinter) {
+//		wprinter.paint();
+//	}
+	//测试打印机是否工作
+	public void testPinter(Printer printer) {
+		printer.paint();
 	}
+	
 }
 
 
@@ -39,8 +52,16 @@ public class TestPrinter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PrintHandle tp = new PrintHandle();
-		tp.testCPinter(1);
-		tp.testCPinter(2);
+//		CPrinter cp = new CPrinter();
+//		tp.testPinter(cp);
+//		WPrinter wp = new WPrinter();
+//		tp.testPinter(wp);
+//		Printer cp = new CPrinter();
+//		tp.testPinter(cp);
+//		Printer wp = new WPrinter();
+//		tp.testPinter(wp);
+		tp.testPinter(new WPrinter());
+		tp.testPinter(new CPrinter());
 		
 	}
 
